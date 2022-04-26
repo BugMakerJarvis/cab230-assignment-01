4790b69cd597fe988ac72f807416c2baf4120c10
Jarvis <shenjiahao.20000115@gmail.com>
2022-04-26 15:28:27 +1000

    feat: layout init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {Layout, Menu, Space} from 'antd';
+import {GithubOutlined, OrderedListOutlined, SmileOutlined} from '@ant-design/icons';
+
+const {Header, Content, Footer} = Layout;
+
+const items = [
+    {key: "Welcome", label: "Welcome", icon: <SmileOutlined/>},
+    {key: "Volcano List", label: "Volcano List", icon: <OrderedListOutlined/>},
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Layout>
+            <Header style={{position: 'fixed', zIndex: 1, width: '100%'}}>
+                <Space direction="horizontal">
+                    <Menu
+                        mode="horizontal"
+                        defaultSelectedKeys={['Welcome']}
+                        items={items}
+                    />
+                </Space>
+            </Header>
+            <Content className="site-layout" style={{padding: '0 50px', marginTop: 64}}>
+                <div className="site-layout-background" style={{padding: 24, minHeight: 380}}>
+                    Content
+                </div>
+            </Content>
+            <Footer style={{textAlign: 'center'}}>
+                <Space direction="vertical">
+                    <GithubOutlined onClick={() => {
+                        window.open("https://github.com/BugMakerJarvis/cab230-assignment-01")
+                    }}/>
+                    <span>CAB230 Assignment-01 ©2022 Created by Jarvis</span>
+                </Space>
+            </Footer>
+        </Layout>
+    );
 }
 
 export default App;
